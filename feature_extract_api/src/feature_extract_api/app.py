4722ed3dcd2420d04ada8379e9b7ae3fbb34c51d
tomfumb <thomaschristian@gmail.com>
2022-09-08 10:19:06 -0700

    Serve docs from root

--- feature_extract_api/src/feature_extract_api/app.py
+++ feature_extract_api/src/feature_extract_api/app.py
@@ -8,11 +8,13 @@ from feature_extract.exceptions.unsupported_dataset import UnsupportedDatasetExc
 from feature_extract.extract_parameters import ExtractParameters
 from feature_extract.retriever import count_features, get_features_file_path
 
-app = FastAPI()
+app = FastAPI(docs_url="/")
 
 
 @app.get("/{dataset}/export/{x_min}/{y_min}/{x_max}/{y_max}")
-async def export(dataset: Dataset, x_min: float, y_min: float, x_max: float, y_max: float) -> FileResponse:
+async def export(
+    dataset: Dataset, x_min: float, y_min: float, x_max: float, y_max: float
+) -> FileResponse:
     return FileResponse(
         get_features_file_path(
             ExtractParameters(
@@ -28,7 +30,9 @@ async def export(dataset: Dataset, x_min: float, y_min: float, x_max: float, y_m
 
 
 @app.get("/{dataset}/count/{x_min}/{y_min}/{x_max}/{y_max}")
-async def count(dataset: Dataset, x_min: float, y_min: float, x_max: float, y_max: float) -> int:
+async def count(
+    dataset: Dataset, x_min: float, y_min: float, x_max: float, y_max: float
+) -> int:
     return count_features(
         ExtractParameters(
             lon_min=x_min,
