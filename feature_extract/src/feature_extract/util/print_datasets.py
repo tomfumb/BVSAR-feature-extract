a84d23a6810ee7b6c5749fb5b11f83c4f66881b2
tomfumb <thomaschristian@gmail.com>
2022-09-18 21:08:09 -0700

    BFE-6 initial work on conversion script using layers

--- feature_extract/src/feature_extract/util/print_datasets.py
+++
@@ -1,7 +0,0 @@
-from feature_extract.common import get_dataset_providers
-
-print(
-    "{}".format(
-        " ".join([f"{provider.get_file_name()}:{provider.get_layer_name()}" for provider in get_dataset_providers()])
-    )
-)
