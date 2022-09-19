e021d41b92174490d71b06a566755502538d2f53
tomfumb <thomaschristian@gmail.com>
2022-09-18 21:08:09 -0700

    BFE-6 initial work on conversion script using layers

---
+++ feature_extract/src/feature_extract/util/print_layers.py
@@ -0,0 +1,3 @@
+from feature_extract.common import get_dataset_providers
+
+print("{}".format(" ".join([provider.get_layer_name() for provider in get_dataset_providers()])))
--- scripts/conversion.sh
+++ scripts/conversion.sh
@@ -3,21 +3,22 @@
 pushd $(dirname $0)/..
 
 docker-compose build
-datasets=($(docker-compose run --rm api python -m feature_extract.util.print_datasets))
+layers=($(docker-compose run --rm api python -m feature_extract.util.print_layers))
 format=FlatGeobuf
 
-for dataset in ${datasets[@]}; do
+for layer in ${layers[@]}; do
 
-    file_name=$(echo $dataset | cut -d ":" -f 1)
-    layer_name=$(echo $dataset | cut -d ":" -f 2)
+    read -p "Path to source file for layer $layer: " source_file_path
+    read -p "Name of source layer for layer $layer [$layer]: " source_layer
 
-    echo "converting ${file_name}:${layer_name} to ${format}"
+    input_mount="$(dirname $source_file_path)"
+    input_file="$(basename $source_file_path)"
 
     docker run \
         --rm \
-        -v ${src_data_dir:-$PWD/feature_extract/data}:/input \
+        -v $input_mount:/input \
         -v ${PWD}/feature_extract/data:/output \
         osgeo/gdal:ubuntu-small-3.5.1 \
-        ogr2ogr -f ${format} /output/${layer_name}.fgb /input/${file_name} ${layer_name}
+        ogr2ogr -f ${format} /output/${layer}.fgb /input/${input_file} ${source_layer:-$layer}
 
 done
