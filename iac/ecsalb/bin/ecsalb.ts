87b5cc1f9197e8cab28a782577c734bef3e0852e
tomfumb <thomaschristian@gmail.com>
2022-09-26 10:19:26 -0700

    BFE-5 added Farget / ECS IAC

--- iac/ecsalb/bin/ecsalb.ts
+++ iac/ecsalb/bin/ecsalb.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { IacStack } from '../lib/iac-stack';
+import { EcsalbStack } from '../lib/ecsalb-stack';
 
 const app = new cdk.App();
-new IacStack(app, 'IacStack', {
+new EcsalbStack(app, 'EcsalbStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
