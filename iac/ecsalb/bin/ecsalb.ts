e034a1ce34df7e0729a9d94ce52519ab1a382d5c
Tyler Battle <tyler@battle.ca>
2022-09-27 10:48:25 -0700

    Add CDK GH workflow

--- iac/ecsalb/bin/ecsalb.ts
+++ iac/ecsalb/bin/ecsalb.ts
@@ -12,6 +12,7 @@ new EcsalbStack(app, 'EcsalbStack', {
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  // env: { account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT }
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
